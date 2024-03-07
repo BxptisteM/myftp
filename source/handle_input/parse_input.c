dccb8dcc76ee5f5aae41c006c7ab5b1baa8cde77
Baptiste Moreau <baptiste.moreau@epitech.eu>
2024-03-07 15:34:11 +0100

    #0 feat: fixed invalid command code, fix line full of spaces

--- source/handle_input/parse_input.c
+++ source/handle_input/parse_input.c
@@ -8,10 +8,28 @@
 #include "commands.h"
 #include <stdlib.h>
 
-void parse_input(char *input, server_t *server, client_t *client)
+static void invalid_command(int i, int array_size, client_t *client)
+{
+    if (i == array_size)
+        write(client->client_socket.fd, "500\r\n", 6);
+}
+
+static char *get_command(char *input)
 {
     char *command = NULL;
     char *tmp = strdup(input);
+
+    command = strtok(tmp, " ");
+    if (command == NULL) {
+        free(tmp);
+        return (strdup("NOCOMMAND\r\n"));
+    }
+    return command;
+}
+
+void parse_input(char *input, server_t *server, client_t *client)
+{
+    char *command = NULL;
     int i = 0;
     int array_size = 0;
     commands_t commands_array[] = {{"USER", &user_cmd}, {"PASS", &pass_cmd},
@@ -23,11 +41,12 @@ void parse_input(char *input, server_t *server, client_t *client)
     if (input == NULL || input[0] == '\0')
         return;
     array_size = sizeof(commands_array) / sizeof(commands_array[0]);
-    command = strtok(tmp, " ");
+    command = get_command(input);
     for (i = 0; i < array_size; ++i) {
         if (strcmp(command, commands_array[i].command) == 0) {
             commands_array[i].func(input, server, client);
             break;
         }
     }
+    invalid_command(i, array_size, client);
 }
