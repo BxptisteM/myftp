179943c76bf2f301639d779624723440741f45e0
Baptiste Moreau <baptiste.moreau@epitech.eu>
2024-03-08 22:33:06 +0100

    #3 feat: first implementation of PASV command

--- source/handle_input/commands/pasv_command.c
+++ source/handle_input/commands/pasv_command.c
@@ -8,9 +8,34 @@
 #include "commands.h"
 #include "client.h"
 
+static void run_passive_mode(client_t *client, int data_socket)
+{
+    struct sockaddr_in data_addr;
+    socklen_t len = sizeof(data_addr);
+
+    memset(&data_addr, 0, sizeof(data_addr));
+    data_addr.sin_family = AF_INET;
+    data_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    data_addr.sin_port = 0;
+    if (bind(data_socket, (struct sockaddr *)&data_addr,
+        sizeof(data_addr)) == -1) {
+        close(data_socket);
+        return;
+    }
+    if (getsockname(data_socket, (struct sockaddr *)&data_addr, &len) == -1) {
+        close(data_socket);
+        return;
+    }
+    client->client_data.fd = data_socket;
+    client->client_data.addr = data_addr;
+    client->client_data.size = len;
+    client->is_passive = true;
+}
+
 void pasv_cmd(char *input UNUSED, server_t *server UNUSED, client_t *client)
 {
     char *extra = NULL;
+    int data_socket = 0;
 
     strtok(input, " \n");
     extra = strtok(NULL, " \n");
@@ -20,4 +45,9 @@ void pasv_cmd(char *input UNUSED, server_t *server UNUSED, client_t *client)
         write(client->client_socket.fd, "501\r\n", 5);
         return;
     }
+    data_socket = socket(AF_INET, SOCK_STREAM, TCP);
+    if (data_socket == -1)
+        return;
+    run_passive_mode(client, data_socket);
+    write(client->client_socket.fd, "227\r\n", 5);
 }
